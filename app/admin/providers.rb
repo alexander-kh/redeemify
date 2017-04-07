64fcfc05e8fa65f5e094f74266f853b694ea5930
alexander_kh <khlipun@gmail.com>
2017-04-07 10:15:29 +0000

    Add RSpec tests for the admin section

--- app/admin/providers.rb
+++ app/admin/providers.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Provider do
   menu :priority => 3
-  permit_params :name, :email
-  
+  permit_params :name, :email, :provider
+
   index do
     column :name
     column :created_at
@@ -10,26 +10,8 @@ ActiveAdmin.register Provider do
     column "# Unclaimed Codes", :unclaimed_codes
     column "# Uploaded Codes", :uploaded_codes
     column "# Removed Codes", :removed_codes
-    column "" do |provider|
-      links = ''.html_safe
-      links += link_to I18n.t('active_admin.edit'), edit_resource_path(provider),
-        :class => "member_link edit_link"
-      links += link_to I18n.t('active_admin.delete'), resource_path(provider),
-        :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'),
-        :class => "member_link delete_link"
-      links
-    end
+    actions
   end
   config.filters = false
-
-  form do |f|
-    f.inputs "Provider Details" do
-      f.input :name
-      f.input :email
-      f.input :provider, label: 'Log in through', :as => :select,
-        :collection => { :Amazon => "amazon", :Google => "google_oauth2",
-        :GitHub => "github", :Twitter => "twitter", :Facebook => "facebook" }
-    end
-    f.actions
-  end
+  form partial: 'admin/form'
 end
